4fb5ca356623318a14940fe6bdd8649db9db0299
yashodhan <2300033084@kluniversity.in>
2025-10-15 21:29:12 +0530

    backend changes

--- backend-jenkins/src/main/java/com/klef/dev/BackendJenkinsApplication.java
+++ backend-jenkins/src/main/java/com/klef/dev/BackendJenkinsApplication.java
@@ -2,16 +2,10 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class BackendJenkinsApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BackendJenkinsApplication.class);
-    }
+public class BackendJenkinsApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendJenkinsApplication.class, args);
