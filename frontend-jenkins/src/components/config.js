1f5a03897cd19aec00fad35c16344d3d49690786
yashodhan <2300033084@kluniversity.in>
2025-10-15 20:53:51 +0530

    changes for docker

--- frontend-jenkins/src/components/config.js
+++ frontend-jenkins/src/components/config.js
@@ -1,6 +1,6 @@
 const config = {
-    url: "http://localhost:2030/backend-jenkins"
-    //url: "http://localhost:2002"
+    //url: "http://localhost:2030/backend-jenkins"
+    url: "http://localhost:2025"
 };
 
-export default config;
+export default config; 
